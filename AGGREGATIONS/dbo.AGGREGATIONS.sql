4f1b25f03956a55ac7d55e3d6228ee485947f74e
Tjitjila Netshiungani <tsmokono@gmail.com>
2026-02-21 13:08:33 +0000

    Push v1

---
+++ AGGREGATIONS/dbo.AGGREGATIONS.sql
@@ -0,0 +1,35 @@
+select * from dbo.[AGGREGATIONS]
+
+SECTION 1 – COUNT Aggregations (10 Questions)
+
+--1. How many total properties are in the database?
+select * from dbo.[AGGREGATIONS]
+
+--2. How many properties are listed in each province?
+
+--3. How many properties are listed in each city?
+
+--4. How many properties have more than 2 bedrooms?
+
+--5. How many properties have 2 or more bathrooms?
+
+--6. How many properties have parking for at least 2 cars?
+
+--7. How many properties are priced above R3,000,000?
+
+--8. How many properties are in Gauteng?
+
+--9. How many properties per province have floor size greater than 200?
+
+--10. How many distinct provinces are in the table?
+--SECTION 2 – SUM Aggregations (10 Questions)
+--11. What is the total value of all properties combined?
+--12. What is the total property value per province?
+--13. What is the total property value per city?
+--14. What is the total monthly repayment for all properties?
+--15. What is the total monthly repayment per province?
+--16. What is the total once-off cost for all properties?
+--17. What is the total once-off cost per province?
+--18. What is the total property value for Gauteng?
+--19. What is the total property value for properties priced above R4,000,000?
+--20. What is the total minimum gross monthly income required per province?
